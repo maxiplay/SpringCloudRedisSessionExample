3f03ffc3eb0c2fd9d37c9ebc920c485636fda88e
Maxime Dhalluin <maxime.dhalluin@gmail.com>
2016-06-11 20:33:25 +0200

    Init oAuth facebook

--- auth-redis-based/src/main/java/com/springcloud/sessionexample/authredisbased/SpringSecurityConfiguration.java
+++ auth-redis-based/src/main/java/com/springcloud/sessionexample/authredisbased/SpringSecurityConfiguration.java
@@ -1,6 +1,15 @@
 package com.springcloud.sessionexample.authredisbased;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.Filter;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,7 +18,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CompositeFilter;
 
 import com.springcloud.sessionexample.authredisbased.rest.RESTAuthenticationEntryPoint;
 import com.springcloud.sessionexample.authredisbased.rest.RESTAuthenticationFailureHandler;
@@ -22,8 +40,12 @@ import com.springcloud.sessionexample.authredisbased.rest.RESTAuthenticationSucc
  *
  */
 @Configuration
+@EnableOAuth2Client
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	OAuth2ClientContext oauth2ClientContext;
+	
 	@Autowired
 	private CsrfService csrfService;
 
@@ -39,6 +61,7 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		
 		http.authorizeRequests()
 				.anyRequest()
 				.permitAll()
@@ -49,7 +72,8 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.csrfTokenRepository(csrfService.csrfTokenRepository())
 				.and()
 				.addFilterAfter(csrfService.csrfHeaderFilter(),
-						CsrfFilter.class);
+						CsrfFilter.class)
+				.addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);;
 
 		http.exceptionHandling().authenticationEntryPoint(
 				authenticationEntryPoint);
@@ -74,4 +98,57 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	public void configure(WebSecurity web) throws Exception {
 		super.configure(web);
 	}
+	
+	
+	
+	
+	@Bean
+	public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(filter);
+		registration.setOrder(-100);
+		return registration;
+	}
+
+
+
+	@Bean
+	@ConfigurationProperties("facebook")
+	ClientResources facebook() {
+		return new ClientResources();
+	}
+
+	private Filter ssoFilter() {
+		CompositeFilter filter = new CompositeFilter();
+		List<Filter> filters = new ArrayList<>();
+		filters.add(ssoFilter(facebook(), "/login/facebook"));
+		filter.setFilters(filters);
+		return filter;
+	}
+
+	private Filter ssoFilter(ClientResources client, String path) {
+		OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter(
+				path);
+		OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(client.getClient(), oauth2ClientContext);
+		facebookFilter.setRestTemplate(facebookTemplate);
+		facebookFilter.setTokenServices(
+				new UserInfoTokenServices(client.getResource().getUserInfoUri(), client.getClient().getClientId()));
+		return facebookFilter;
+	}
+
+}
+
+class ClientResources {
+	private OAuth2ProtectedResourceDetails client = new AuthorizationCodeResourceDetails();
+	private ResourceServerProperties resource = new ResourceServerProperties();
+
+	public OAuth2ProtectedResourceDetails getClient() {
+		return client;
+	}
+
+	public ResourceServerProperties getResource() {
+		return resource;
+	}
+	
+	
 }
--- user-data-model/src/main/java/com/springcloud/sessionexample/userdatamodel/repository/AuthController.java
+++ user-data-model/src/main/java/com/springcloud/sessionexample/userdatamodel/repository/AuthController.java
@@ -1,11 +1,13 @@
 package com.springcloud.sessionexample.userdatamodel.repository;
 
 import java.security.Principal;
+import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,12 +29,29 @@ public class AuthController {
 
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private UserRepository userRepository;
 
 	@JsonView(UserView.Protected.class)
 	@ResponseBody
 	@RequestMapping(value = "/user", method = RequestMethod.GET)
 	public ResponseEntity<User> getUser(Principal user, HttpSession session) {
 		System.out.println(user);
+		
+		if(user instanceof OAuth2Authentication){
+			
+			OAuth2Authentication oAuthUser = (OAuth2Authentication) user;
+			if(oAuthUser.isAuthenticated()){
+				
+				Optional<User> mongoUser = userRepository.findOneByLogin("max3");
+				
+				return ResponseEntity.ok().body(mongoUser.get());
+				
+			}
+			
+		}
+		
+		
 		return ResponseEntity.ok().body(userService.getMongoUser(user));
 	}
 
